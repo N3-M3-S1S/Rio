74ae6a9d89d7b9bdb32494e730f61f99c32830d3
nemesis <nemesis.mk47@gmail.com>
2021-11-22 17:47:37 +0300

    Add enumMapOf function

--- utils/src/main/java/com/nemesis/rio/utils/EnumUtils.kt
+++ utils/src/main/java/com/nemesis/rio/utils/EnumUtils.kt
@@ -1,8 +1,16 @@
 package com.nemesis.rio.utils
 
-import java.util.EnumMap
+import java.util.*
 
 inline fun <reified K : Enum<K>, V> enumMap() = EnumMap<K, V>(K::class.java)
+
+inline fun <reified K : Enum<K>, V> enumMapOf(vararg pairs: Pair<K, V>): EnumMap<K, V> {
+    val map = enumMap<K, V>()
+    for (pair in pairs) {
+        map[pair.first] = pair.second
+    }
+    return map
+}
 
 inline fun <reified T : Enum<T>> searchEnumByName(name: String, ignoreCase: Boolean = true): T? =
     enumValues<T>().find { it.name.equals(name, ignoreCase) }
