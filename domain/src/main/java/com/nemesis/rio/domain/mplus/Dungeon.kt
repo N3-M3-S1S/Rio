651689e98de1deb8c6a2de45bee7606ebf785ced
nemesis <nemesis.mk47@gmail.com>
2021-11-28 13:01:43 +0300

    Update dungeon timers

--- domain/src/main/java/com/nemesis/rio/domain/mplus/Dungeon.kt
+++ domain/src/main/java/com/nemesis/rio/domain/mplus/Dungeon.kt
@@ -2,13 +2,13 @@ package com.nemesis.rio.domain.mplus
 
 import java.time.Duration
 
-enum class Dungeon(val duration: Duration) {
+enum class Dungeon(val completionTimer: Duration) {
     DE_OTHER_SIDE(Duration.ofMinutes(43)),
     MISTS_OF_THIRNA_SCITHE(Duration.ofMinutes(30)),
     SANGUINE_DEPTHS(Duration.ofMinutes(41)),
-    HALLS_OF_ATONEMENT(Duration.ofMinutes(31)),
+    HALLS_OF_ATONEMENT(Duration.ofMinutes(32)),
     NECROTIC_WAKE(Duration.ofMinutes(36)),
     SPIRES_OF_ASCENSION(Duration.ofMinutes(39)),
-    THEATER_OF_PAIN(Duration.ofMinutes(37)),
+    THEATER_OF_PAIN(Duration.ofMinutes(38)),
     PLAGUEFALL(Duration.ofMinutes(38))
 }
